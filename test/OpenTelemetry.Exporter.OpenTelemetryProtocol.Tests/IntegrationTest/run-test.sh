bc149ba10c828eff914bdecc707a72455f6ed435
SophCarp <sophiacarpenter@gmail.com>
2024-04-12 12:51:05 -0700

    Deleted all but examples

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Trust the self-signed certificated used by the collector
-cp /cfg/otel-collector.crt /usr/local/share/ca-certificates/
-update-ca-certificates --verbose
-
-dotnet test OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests.dll --TestCaseFilter:CategoryName=CollectorIntegrationTests --logger "console;verbosity=detailed"
